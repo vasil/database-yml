db74c143af469f91753cc5d94461216844ebbd34
Vasil Taneski <vasil@taneski.com>
2009-12-20 21:49:04 +0100

    now database config file can be set relatively

--- lib/sinatra/database-yml.rb
+++ lib/sinatra/database-yml.rb
@@ -3,14 +3,13 @@ require "dm-core"
 
 module Sinatra
   module DatabaseYml
-
     class Configuration
       attr_accessor :database_configuration_file
 
       def initialize
         self.database_configuration_file  =
           Sinatra::Application.respond_to?(:database_configuration_file) ?
-          Sinatra::Application.database_configuration_file :
+          File.join(Sinatra::Application.root, database_configuration_file) :
           default_database_configuration_file
       end
 
@@ -29,32 +28,17 @@ module Sinatra
       def configuration
         @@configuration
       end
-
       def configuration=(configuration)
         @@configuration = configuration
-      end
-
-      def initialize_database
-        DataMapper.setup(:default,
-          configuration.database_configuration[Sinatra::Application.environment.to_s])
-        DataMapper.auto_upgrade!(:default)
-      end
-
-      def initialize_database_logger
-        if DataMapper::Repository.adapters[:default]
-          case Sinatra::environment
-          when :development then DataMapper::Logger.new(STDOUT,  :debug)
-          when :production  then DataMapper::Logger.new(LOGFILE, :warning)
-          end
-        end
       end
     end
 
     self.configuration = Configuration.new
 
     Sinatra::Application.configure do
-      initialize_database
+      DataMapper.setup(:default,
+          configuration.database_configuration[Sinatra::Application.environment.to_s])
+      DataMapper.auto_upgrade!(:default)
     end
-
   end
 end
